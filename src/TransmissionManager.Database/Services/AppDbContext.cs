6167267999a3a02e5efbbaf825a43b12accbefe9
Andrii Annenko <roxton.public@gmail.com>
2024-11-15 13:26:21 +0100

    Fix unwanted trimming of EF Core internal class members

--- src/TransmissionManager.Database/Services/AppDbContext.cs
+++ src/TransmissionManager.Database/Services/AppDbContext.cs
@@ -1,8 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using System.Diagnostics.CodeAnalysis;
 using TransmissionManager.Database.Models;
 
 namespace TransmissionManager.Database.Services;
 
+[method: DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(EntryCurrentValueComparer<>))]
+#pragma warning disable EF1001 // Internal EF Core API usage - required to prevent required class members from being trimmed
+[method: DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(NullableClassCurrentProviderValueComparer<,>))]
+#pragma warning restore EF1001 // Internal EF Core API usage.
 #pragma warning disable IL2026 // Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code - tested after trimming
 public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 #pragma warning restore IL2026 // Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code
