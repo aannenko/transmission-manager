7f78faefca3365cb52f73aefb4c803f3b55ed0fe
Andrii Annenko <aannenko@users.noreply.github.com>
2025-09-01 07:52:44 +0000

    Simplify regex in CronAttribute

--- src/TransmissionManager.Api.Common/Attributes/CronAttribute.cs
+++ src/TransmissionManager.Api.Common/Attributes/CronAttribute.cs
@@ -5,7 +5,7 @@ namespace TransmissionManager.Api.Common.Attributes;
 public sealed class CronAttribute : RegularExpressionAttribute
 {
     private const string _isCron =
-        @"^((\*(/\d{1,2})?|\d{1,2}(/\d{1,2})?|(\d{1,2}-\d{1,2})(/\d{1,2})?|((\d{1,2},)+\d{1,2}))\s){4}(\*(/\d{1,2})?|\d{1,2}(/\d{1,2})?|(\d{1,2}-\d{1,2})(/\d{1,2})?|((\d{1,2},)+\d{1,2}))$";
+        @"^((\*(\d{1,2})?|\d{1,2}(\d{1,2})?|(\d{1,2}-\d{1,2})(\d{1,2})?|((\d{1,2},)+\d{1,2}))\s){4}(\*(\d{1,2})?|\d{1,2}(\d{1,2})?|(\d{1,2}-\d{1,2})(\d{1,2})?|((\d{1,2},)+\d{1,2}))$";
 
     public CronAttribute() : base(_isCron)
     {
