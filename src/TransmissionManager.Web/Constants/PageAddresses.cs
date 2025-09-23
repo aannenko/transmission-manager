5532a37d7889d614f9553f5102907499f8f53959
Andrii Annenko <roxton.public@gmail.com>
2025-09-23 11:53:52 +0200

    Make the home NavTab route to root instead of torrents

--- src/TransmissionManager.Web/Constants/PageAddresses.cs
+++ src/TransmissionManager.Web/Constants/PageAddresses.cs
@@ -2,8 +2,8 @@
 
 internal static class PageAddresses
 {
-    public const string Home = "/";
+    public const string Home = "";
     public const string Torrents = "/torrents";
-    public const string AddTorrent = "/torrents/add";
+    public const string TorrentsAdd = "/torrents/add";
     public const string Connect = "/connect";
 }
